7eee360bb2724dd5570f062e27105e8a58543c2a
Ankush Sood <ankush.s@voyantcs.com>
2024-09-20 02:25:40 +0530

    readme and cron added to console.php

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Console\Scheduling\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('schedule-tasks', function (Schedule $schedule) {
+    $schedule->command('app:update-not-returned-rentals-as-over-due')->dailyAt('00:00');
+    $schedule->command('send:overdue-emails-to-users')->dailyAt('00:30');
+})->describe('Schedules tasks to run and update the status of overdue rentals and send notifications to the user via email');
